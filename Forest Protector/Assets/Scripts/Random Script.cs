1d8c252e235dce83af6a8497f9d876e6535a12b6
GithinGeorge <githin2k1@pop-os.localdomain>
2022-01-27 17:38:33 +0530

    Animations added for 2 characters

--- Forest Protector/Assets/Scripts/Random Script.cs
+++ Forest Protector/Assets/Scripts/Random Script.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class RandomScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public int SpawnerLimit = 10;
+    public GameObject[] Sprites;
+    // Enemy 1, Enemy 2 Enemy 3..
+
+    // Random Selection of one prefab and then instantiate if count<Limit || if it doenst collide with another enemy or player...
+
+    // Layering 
+
     void Start()
     {
     }
