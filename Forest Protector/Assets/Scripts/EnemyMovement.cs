9bd61ad0b61c2db0453fe54d42b16034369ccfa9
roopasree123 <cs19b008@iittp.ac.in>
2022-01-28 12:30:20 +0530

    enemy movement script

--- Forest Protector/Assets/Scripts/EnemyMovement.cs
+++ Forest Protector/Assets/Scripts/EnemyMovement.cs
@@ -4,17 +4,59 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float speed=20f;
-    public Rigidbody2D myBody;
-    void Awake()
+    public float myspeed = 3f;
+    Transform leftWayPoint, rightWayPoint;
+    Vector3 localScale;
+    bool movingright = true;
+    public Rigidbody2D mybody;
+    void Start()
     {
-        myBody = GetComponent<Rigidbody2D>();
+        localScale = transform.localScale;
+        mybody = GetComponent<Rigidbody2D>();
+        leftWayPoint = GameObject.Find("LeftWayPoint").GetComponent<Transform>();
+        rightWayPoint = GameObject.Find("rightWayPoint").GetComponent<Transform>();
+
+    }
+    void Update()
+    {
+        if (transform.position.x > rightWayPoint.position.x)
+            movingright = false;
+        if (transform.position.x < leftWayPoint.position.x)
+            movingright = true;
+
+        if (movingright)
+            moveRight();
+        else
+            moveLeft();
+    }
+    void moveRight()
+    {
+        movingright = true;
+        localScale.x = 1;
+        transform.localScale = localScale;
+        mybody.velocity = new Vector2(localScale.x * myspeed, mybody.velocity.y);
+    }
+    void moveLeft()
+    {
+        movingright = false;
+        localScale.x = -1;
+        transform.localScale = localScale;
+        mybody.velocity = new Vector2(localScale.x * myspeed, mybody.velocity.y);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        myBody.velocity = new Vector2(speed, myBody.velocity.y);
-    }
+
+    // public float speed = 20f;
+    // public Rigidbody2D myBody;
+    // void Awake()
+    // {
+    //     myBody = GetComponent<Rigidbody2D>();
+    // }
+
+    // // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     myBody.velocity = new Vector2(speed, myBody.velocity.y);
+    // }
+
 }
 
