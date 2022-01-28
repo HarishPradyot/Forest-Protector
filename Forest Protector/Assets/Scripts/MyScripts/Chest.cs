32941e91384a6da7d51b27b9e226ff90ce4a5a13
Harish Pradyot 7 <harishpradyot@gmail.com>
2022-01-28 13:45:10 +0530

    Chest Interaction and Animation Complete and Small Changes

--- Forest Protector/Assets/Scripts/MyScripts/Chest.cs
+++ Forest Protector/Assets/Scripts/MyScripts/Chest.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Chest : MonoBehaviour
 {
+    [SerializeField]
+    private float waitDuration=4f;  // Time for which chest should remain opened before closing again
+    private Animator chestAnimator;
+    private string OPEN_CHEST, CLOSE_CHEST;
     // Start is called before the first frame update
     void Start()
     {
-        
+        chestAnimator=GetComponent<Animator>();
+        OPEN_CHEST="Base Layer.Chest Open";
+        CLOSE_CHEST="Base Layer.Chest Close";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void openChest()
+    {
+        Debug.Log("Long Presss Working");
+        chestAnimator.Play(OPEN_CHEST);
+        StartCoroutine(closeChest());
+    }
+    IEnumerator closeChest()
+    {
+        float startTime=Time.time;
+        while(Time.time-startTime<=waitDuration)
+            yield return null;
+        chestAnimator.Play(CLOSE_CHEST);
     }
 }
