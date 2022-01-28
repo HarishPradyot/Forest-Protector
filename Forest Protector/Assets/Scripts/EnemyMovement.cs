251356dbfad087f309ccf1ad1f5c5e98780c43f0
roopasree123 <cs19b008@iittp.ac.in>
2022-01-28 17:56:32 +0530

    enemy movement initiated

--- Forest Protector/Assets/Scripts/EnemyMovement.cs
+++ Forest Protector/Assets/Scripts/EnemyMovement.cs
@@ -1,62 +1,54 @@
+// using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float myspeed = 3f;
-    Transform leftWayPoint, rightWayPoint;
-    Vector3 localScale;
-    bool movingright = true;
-    public Rigidbody2D mybody;
+
+    public float moveSpeed;
+    public Vector3 dir;
+    public float turnSpeed;
+    float targetAngle;
+    Vector3 currentPos;
+    bool play = true;
+    Vector3 direction;
     void Start()
     {
-        localScale = transform.localScale;
-        mybody = GetComponent<Rigidbody2D>();
-        leftWayPoint = GameObject.Find("LeftWayPoint").GetComponent<Transform>();
-        rightWayPoint = GameObject.Find("rightWayPoint").GetComponent<Transform>();
-
+        dir = Vector3.up;
+        InvokeRepeating("Start1", 0f, 5f);
+    }
+    void Start1()
+    {
+        play = true;
+        direction = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-4.0f, 4.0f), 0); //random position in x and y
     }
     void Update()
     {
-        if (transform.position.x > rightWayPoint.position.x)
-            movingright = false;
-        if (transform.position.x < leftWayPoint.position.x)
-            movingright = true;
+        currentPos = transform.position;//current position of gameObject
+        if (play)
+        { //calculating direction
+            dir = direction - currentPos;
 
-        if (movingright)
-            moveRight();
-        else
-            moveLeft();
+            dir.z = 0;
+            dir.Normalize();
+            play = false;
+        }
+        Vector3 target = dir * moveSpeed + currentPos;  //calculating target position
+        transform.position = Vector3.Lerp(currentPos, target, Time.deltaTime);//movement from current position to target position
+        targetAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90; //angle of rotation of gameobject
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), turnSpeed * Time.deltaTime); //rotation from current direction to target direction
     }
-    void moveRight()
+    void OnCollisionEnter2D()
     {
-        movingright = true;
-        localScale.x = 1;
-        transform.localScale = localScale;
-        mybody.velocity = new Vector2(localScale.x * myspeed, mybody.velocity.y);
+
+        CancelInvoke();//stop call to start1 method
+        direction = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-4.0f, 4.0f), 0); //again provide random position in x and y
+        play = true;
+
     }
-    void moveLeft()
+    void OnCollisionExit2D()
     {
-        movingright = false;
-        localScale.x = -1;
-        transform.localScale = localScale;
-        mybody.velocity = new Vector2(localScale.x * myspeed, mybody.velocity.y);
+        InvokeRepeating("Start1", 2f, 5f);
     }
-
-
-    // public float speed = 20f;
-    // public Rigidbody2D myBody;
-    // void Awake()
-    // {
-    //     myBody = GetComponent<Rigidbody2D>();
-    // }
-
-    // // Update is called once per frame
-    // void FixedUpdate()
-    // {
-    //     myBody.velocity = new Vector2(speed, myBody.velocity.y);
-    // }
-
 }
-
